3f4ddabd9fe8f15d664312dc7291e8c55f124418
Jakob <jakob1379@gmail.com>
2019-04-05 20:33:26 +0200

    Possible with several paths in cfg
    Now it is possible to make comma separated paths in .cfg

--- dmenu_hotkeys/config.py
+++ dmenu_hotkeys/config.py
@@ -6,3 +6,8 @@ def get_config():
     cfg = ConfigParser()
     cfg.read_string(resources.read_text("dmenu_hotkeys", "config.cfg"))
     return cfg
+
+def get_config2():
+    cfg = ConfigParser()
+    cfg.read_file(open(r"config.cfg"))
+    return cfg
